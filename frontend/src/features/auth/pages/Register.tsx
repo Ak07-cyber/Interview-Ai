0a431fba45a0bb999b5a3857d46ce5ed889fd1d9
Ayush Kumar Jha <70813709+Ak07-cyber@users.noreply.github.com>
2026-03-23 01:34:57 +0530

    Fix label text for password input in Register component

--- frontend/src/features/auth/pages/Register.tsx
+++ frontend/src/features/auth/pages/Register.tsx
@@ -41,7 +41,7 @@ const Register = () => {
                     id='email' name='email' type='text' placeholder='enter your email'/>
                 </div>
                 <div className='input-group'>
-                    <label htmlFor='password'>email</label>
+                    <label htmlFor='password'>Password</label>
                     <input
                     onChange={(e)=>{setPassword(e.target.value)}}
                     id='password' name='password' type='password' placeholder='enter your password'/>
